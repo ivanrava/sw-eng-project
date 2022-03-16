dff16cd4cc6a130590602f9206afa02f6b92bd60
AlexMarchi2000 <44038580+AlexMarchi2000@users.noreply.github.com>
2022-03-16 18:02:37 +0100

    Migliorato ToString Enum

--- src/it/unibs/ing/ingsw/article/ArticleState.java
+++ src/it/unibs/ing/ingsw/article/ArticleState.java
@@ -3,6 +3,18 @@ package it.unibs.ing.ingsw.article;
 import java.io.Serializable;
 
 public enum ArticleState implements Serializable {
-    OFFERTA_APERTA,
-    OFFERTA_RITIRATA
+    OFFERTA_APERTA("Offerta Aperta"),
+    OFFERTA_RITIRATA("Offerta Ritirata");
+
+    private final String text;
+
+    ArticleState(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
+
