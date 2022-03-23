1fc1d7f793281c0dc5237054445f049dfe5a56f7
AlexMarchi2000 <44038580+AlexMarchi2000@users.noreply.github.com>
2022-03-23 11:46:18 +0100

    Aggiunta visualizzazione Baratti

--- src/it/unibs/ing/ingsw/ui/CustomerView.java
+++ src/it/unibs/ing/ingsw/ui/CustomerView.java
@@ -2,9 +2,9 @@ package it.unibs.ing.ingsw.ui;
 
 import it.unibs.ing.fp.mylib.MyMenu;
 import it.unibs.ing.ingsw.article.ArticleView;
+import it.unibs.ing.ingsw.article.ExchangeView;
 import it.unibs.ing.ingsw.auth.User;
 import it.unibs.ing.ingsw.category.CategoryView;
-import it.unibs.ing.ingsw.config.Config;
 import it.unibs.ing.ingsw.config.ConfigView;
 import it.unibs.ing.ingsw.io.Saves;
 
@@ -13,14 +13,17 @@ public class CustomerView {
     private static final String SEE_CATEGORIES = "Visualizza categorie";
     private static final String SEE_CONFIG = "Visualizza generalità";
     public static final String MANAGE_ARTICLES = "Gestisci gli articoli";
+    public static final String MANAGE_EXCHANGES = "Gestisci i baratti";
     private final CategoryView categoryView;
     private final ConfigView configView;
     private final ArticleView articleView;
+    private final ExchangeView exchangeView;
 
     public CustomerView(Saves saves) {
         categoryView = new CategoryView(saves);
         configView = new ConfigView(saves);
         articleView = new ArticleView(saves);
+        exchangeView = new ExchangeView(saves);
     }
 
     /**
@@ -30,7 +33,8 @@ public class CustomerView {
         MyMenu mainMenu = new MyMenu(MENU_TITLE, new String[] {
                 SEE_CATEGORIES,
                 SEE_CONFIG,
-                MANAGE_ARTICLES
+                MANAGE_ARTICLES,
+                MANAGE_EXCHANGES
         });
 
         int scelta;
@@ -40,6 +44,7 @@ public class CustomerView {
                 case 1 -> categoryView.printHierarchies();
                 case 2 -> configView.printConfig();
                 case 3 -> articleView.execute(user);
+                case 4 -> exchangeView.execute(user);
             }
         }while (scelta != 0);
     }
