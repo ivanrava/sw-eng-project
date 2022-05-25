dd4139578b52d232b2446b4f2a0845330520bf3e
Alex <alex.marchi2000@gmail.com>
2022-05-25 10:28:45 +0200

    removed unused imports from Main.java

--- src/it/unibs/ing/ingsw/Main.java
+++ src/it/unibs/ing/ingsw/Main.java
@@ -1,12 +1,9 @@
 package it.unibs.ing.ingsw;
 
-import it.unibs.ing.ingsw.io.batch.JsonParser;
 import it.unibs.ing.ingsw.io.Saves;
 import it.unibs.ing.ingsw.ui.View;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
     private static final String ERROR_LOAD_CONFIG = "La configurazione è presente, ma non riesco a caricarla!";
