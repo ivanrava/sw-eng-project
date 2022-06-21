699b67951f84d1f5e34e4bee809d34bad953e80f
Alex <alex.marchi2000@gmail.com>
2022-06-21 11:51:58 +0200

    reformat code

--- src/it/unibs/ing/ingsw/article/ArticleController.java
+++ src/it/unibs/ing/ingsw/article/ArticleController.java
@@ -20,6 +20,7 @@ public class ArticleController {
 
     /**
      * Costruttore parametrizzato
+     *
      * @param saves Salvataggi dell'applicazione
      */
     public ArticleController(DataContainer saves) {
@@ -34,16 +35,17 @@ public class ArticleController {
 
     /**
      * Fattorizzazione del controllo di appartenenza ad un utente
+     *
      * @param username Username per il controllo
      * @return Lo stream di articoli per quell'utente
      */
     private Stream<Article> getStreamArticlesForUser(String username) {
-        return articles.values().stream()
-                .filter(article -> article.getOwner().getUsername().equals(username));
+        return articles.values().stream().filter(article -> article.getOwner().getUsername().equals(username));
     }
 
     /**
      * Ritorna una lista di articoli appartenenti ad un certo utente
+     *
      * @param username Username dell'utente
      * @return Lista di articoli di quell'utente
      */
@@ -53,66 +55,61 @@ public class ArticleController {
 
     /**
      * Ritorna una lista degli articoli disponibili per il baratto
+     *
      * @param username Username
      * @return Lista degli articoli di un utente disponibili per il baratto
      */
     public List<Article> getArticlesAvailableForUser(String username) {
-        return getStreamArticlesForUser(username)
-                .filter(Article::isAvailable)
-                .toList();
+        return getStreamArticlesForUser(username).filter(Article::isAvailable).toList();
     }
 
     /**
      * Ritorna una lista degli articoli modificabili nel frontend
+     *
      * @param username Username
      * @return Lista degli articoli modificabili nel frontend
      */
     public List<Article> getArticlesEditableForUser(String username) {
-        return getStreamArticlesForUser(username)
-                .filter(Article::isEditable)
-                .toList();
+        return getStreamArticlesForUser(username).filter(Article::isEditable).toList();
     }
 
     /**
      * @param categoryRootName Nome della categoria radice
      * @param categoryLeafName Nome della categoria foglia
-     * @param isAdmin Indica se ricevere i risultati destinati agli utenti privilegiati oppure agli utenti semplici
+     * @param isAdmin          Indica se ricevere i risultati destinati agli utenti privilegiati oppure agli utenti semplici
      * @return Lista di articoli di una categoria foglia
      */
     public List<Article> getArticlesForCategory(String categoryRootName, String categoryLeafName, boolean isAdmin) {
         Category category = categoryController.searchTree(categoryRootName, categoryLeafName);
-        return articles.values().stream()
-                .filter(article -> article.getCategory().equals(category))
-                .filter(article -> {
-                    ArticleState state = article.getState();
-                    if (isAdmin) {
-                        return state.equals(ArticleState.OFFERTA_APERTA) ||
-                                state.equals(ArticleState.OFFERTA_SCAMBIO) ||
-                                state.equals(ArticleState.OFFERTA_CHIUSA);
-                    } else {
-                        return state.equals(ArticleState.OFFERTA_APERTA);
-                    }
-                })
-                .toList();
+        return articles.values().stream().filter(article -> article.getCategory().equals(category)).filter(article -> {
+            ArticleState state = article.getState();
+            if (isAdmin) {
+                return state.equals(ArticleState.OFFERTA_APERTA) || state.equals(ArticleState.OFFERTA_SCAMBIO) || state.equals(ArticleState.OFFERTA_CHIUSA);
+            } else {
+                return state.equals(ArticleState.OFFERTA_APERTA);
+            }
+        }).toList();
     }
 
     /**
      * Aggiunge un articolo
-     * @param username Username dell'utente che lo possiede
+     *
+     * @param username         Username dell'utente che lo possiede
      * @param rootCategoryName Nome della categoria radice della categoria dell'articolo
      * @param leafCategoryName Nome della categoria foglia dell'articolo
-     * @param articleState Stato dell'articolo
-     * @param fieldValues Valori dei campi della categoria foglia
+     * @param articleState     Stato dell'articolo
+     * @param fieldValues      Valori dei campi della categoria foglia
      */
     public void addArticle(String username, String rootCategoryName, String leafCategoryName, ArticleState articleState, Map<String, String> fieldValues) {
         User user = userController.getUserByUsername(username);
         Category leafCategory = categoryController.searchTree(rootCategoryName, leafCategoryName);
         assert fieldValues.keySet().equals(leafCategory.getFields().keySet()) : ASSERTION_DIFFERENT_FIELDS;
-        articles.put(articles.size()+1, new Article(articles.size()+1, user, leafCategory, articleState, fieldValues));
+        articles.put(articles.size() + 1, new Article(articles.size() + 1, user, leafCategory, articleState, fieldValues));
     }
 
     /**
      * Controlla che esista un articolo con un certo id
+     *
      * @param id Identificatore numerico dell'articolo
      * @return 'true' se esiste, 'false' altrimenti
      */
@@ -122,6 +119,7 @@ public class ArticleController {
 
     /**
      * Dice se l'articolo passato è modificabile dal frontend
+     *
      * @param id Id dell'articolo
      * @return 'true' se modificabile, 'false' altrimenti
      */
@@ -140,7 +138,8 @@ public class ArticleController {
 
     /**
      * Aggiorna lo stato dell'articolo
-     * @param id Identificatore numerico dell'articolo
+     *
+     * @param id              Identificatore numerico dell'articolo
      * @param newArticleState Nuovo stato dell'articolo
      */
     public void updateState(int id, ArticleState newArticleState) {
@@ -150,14 +149,13 @@ public class ArticleController {
 
     /**
      * Ritorna gli articoli disponibili per il baratto
-     * @param user Utente che richiede lo scambio
+     *
+     * @param user     Utente che richiede lo scambio
      * @param category Categoria in cui avviene lo scambio
      * @return Articoli disponibili per il baratto
      */
     public List<Article> getAvailableArticlesForExchange(User user, Category category) {
         assert category.isLeaf() : ASSERTION_CATEGORY_NOT_LEAF;
-        return articles.values().stream()
-                .filter(article -> article.getCategory().equals(category) && article.isAvailable() && !article.getOwner().equals(user))
-                .toList();
+        return articles.values().stream().filter(article -> article.getCategory().equals(category) && article.isAvailable() && !article.getOwner().equals(user)).toList();
     }
 }
