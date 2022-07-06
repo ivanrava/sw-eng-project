489f7e65c61be003e7fcb416c3eca7adf3d09f35
Ivan Ravasi <ivan4.ravasi@gmail.com>
2022-07-06 13:56:01 +0200

    Ottimizza gli import

--- src/it/unibs/ing/tests/feature/LoginTest.java
+++ src/it/unibs/ing/tests/feature/LoginTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
